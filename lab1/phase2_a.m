666dc6e6180c2b97aeaa8152982beb84427b3356
TheApo98 <apogioum.ag@gmail.com>
2021-03-22 21:42:20 +0200

    Added tuned P(ID) controllers

--- lab1/phase2_a.m
+++ lab1/phase2_a.m
@@ -1,13 +1,13 @@
 clear all; close all; clc;
 
-%% Ziegler-Nichols Method
+% Ziegler-Nichols Method
 
 Ks = 1;
 T1 = 2;
 T2 = 2;
 T3 = 2;
 
-%% Tranfer Function and unit feedback
+% Tranfer Function and unit feedback
 
 tmp1 = tf(1, [T1 1]);
 tmp2 = tf(1, [T2 1]);
@@ -64,7 +64,7 @@ T_crit = max(diff(locs));
 
 fprintf('K_critical: %.1f. \nT_critical: %.3f seconds. \n\n', Kp_crit, T_crit);
 
-%% Display plots that helped.
+% Display plots that helped.
 
 % To crate dinamically a matrix of plots in the same figure
 % See numSubplots.m for more info.
@@ -85,7 +85,7 @@ for i=1:length(plants)
     hold on;
 end
 
-%% P,PI,PID calculation using ZN method
+% P,PI,PID calculation using ZN method
 % We have now gathered the values we want to create the control system
 % Below is the implementation of the Ziegler-Nichols model.
 
@@ -94,6 +94,10 @@ Kp1 = 0.5*Kp_crit;
 %T_max = 100;  % random value
 PID_controller = pidstd(Kp1);  
 p_step = feedback(sys1*PID_controller,1);
+%optimized controller
+Options = pidtuneOptions('PhaseMargin',60);
+opt_c1 = pidtune(sys1, PID_controller, Options);
+opt_p_step = feedback(sys1*opt_c1,1);
 
 % PI Control
 Kp2 = 0.45*Kp_crit;
@@ -101,6 +105,10 @@ Ti1 = 0.85*T_crit;
 % T_max = 100;  % random value
 PID_controller = pidstd(Kp2,Ti1);  
 pi_step = feedback(sys1*PID_controller,1);
+%optimized controller
+Options = pidtuneOptions('PhaseMargin',69);
+opt_c1 = pidtune(sys1, PID_controller, Options);
+opt_pi_step = feedback(sys1*opt_c1,1);
     
 % PID Control
 Kp3 = 0.6*Kp_crit;
@@ -108,9 +116,14 @@ Ti2 = 0.5*T_crit;
 Td = 0.12*T_crit;
 % T_max = 30*Td;
 PID_controller = pidstd(Kp3,Ti2,Td);
+opt_pid_c = pidstd(0.77,4.31,1.07);    %optimal pid ctrl
 pid_step = feedback(sys1*PID_controller,1);
+%optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+opt_c1 = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*opt_c1,1);
 
-%% Plot controlers
+% Plot controlers
 figure('Name','TF of P,PI,PID controlers');
 step([0:100],sys1,p_step,pi_step,pid_step);
 %legend(['Sys1','P_stepKp (Kp = ' num2str(Kp1) ')'] ,['PI_step (Kp = ' num2str(Kp2) ', Ki = ' num2str(Ti1) ')'] ,['PID_step (Kp = ' num2str(Kp3) ',Ki = ' num2str(Ti2) ',Kd = ' num2str(Td) ')']);
@@ -123,22 +136,29 @@ figure('Name','TF of P,PI,PID controlers');
 subplot(3,1,1);
 step(p_step);
 hold on;
+step(opt_p_step)
+hold on
 step(sys1);
 title(['P controler (Kp = ' num2str(Kp1) ').']);
-legend('P_step','Sys1');
+legend('P_step','Tuned_P_step','Sys1');
 hold on;
+
 
 subplot(3,1,2);
 step(pi_step);
 hold on;
+step(opt_pi_step);
+hold on
 step(sys1);
 title(['PI controler (Kp = ' num2str(Kp2) ', Ki = ' num2str(Ti1) ').']);
-legend('PI_step','Sys1');
+legend('PI_step','Tuned_PI_step','Sys1');
 hold on;
 
 subplot(3,1,3);
 step(pid_step);
 hold on;
+step(opt_pid_step);
+hold on
 step(sys1);
 title(['PID controler (Kp = ' num2str(Kp3) ', Ki = ' num2str(Ti2) ', Kd = ' num2str(Td) ').']);
-legend('PID_step','Sys1');
+legend('PID_step','Tuned_PID_step','Sys1');
