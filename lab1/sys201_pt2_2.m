50e52fc557df4f59293c82c942c95af5beff82fe
TheApo98 <apogioum.ag@gmail.com>
2021-03-22 23:19:28 +0200

    Added some plots and tuned (not optimally) the controllers

--- lab1/sys201_pt2_2.m
+++ lab1/sys201_pt2_2.m
@@ -18,72 +18,143 @@ sys1 = Ks*tmp1*tmp2*tmp3;
 
 M = feedback(sys1, 1);
 
-figure(1)
-subplot(2,1,1),
-step(sys1)
-subplot(2,1,2),
-step(M)
-title('Feedback loop step response')
+figure('Name','Transfer function & feedback')
+subplot(2,1,1);
+step(sys1);
+title('Step Response.');
+xlabel('Time');
+hold on; 
+subplot(2,1,2);
+step(M);
+title('Step Response with unit feedback');
+xlabel('Time');
 
+%parameters
 K = 1;
 Te = 1.7;     %tu
 Tb = 6.7;   %tg
 
 
-% ********* 0% overshoot ***********
-%p controller
-Kp = (0.3*Tb)/(K*Te);
-pid_ctrl = pidstd(Kp)
+%% ********* 0% overshoot ***********
+fprintf('\t\t0 percent overshoot\n');
+        %p controller
+%///Parameters
+Kp1 = (0.3*Tb)/(K*Te);
+%\\\
+PID_controller = pidstd(Kp1)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 figure('name', '0% overshoot')
+fprintf('\nSystem TF');
 subplot(3,1,1),
-m = feedback(pid_ctrl*sys1,1);
-step(m);
-title('P controller')
+m1 = feedback(PID_controller*sys1,1)
+step(m1);
+hold on; step(opt_pid_step); hold on;
+step(sys1);
+title(['P controler (Kp = ' num2str(Kp1) ').']);
+legend('P_step','Tuned_P_step','Sys1');
 
-%pi controller
-Kp = (0.35*Tb)/(K*Te);
-Ti = 1.2*Tb;
-pid_ctrl = pidstd(Kp, Ti)
+
+        %pi controller
+%///Parameters
+Kp2 = (0.35*Tb)/(K*Te);
+Ti1 = 1.2*Tb;
+%\\\
+PID_controller = pidstd(Kp2, Ti1)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 subplot(3,1,2),
-m = feedback(pid_ctrl*sys1,1);
-step(m);
-title('PI controller')
+m2 = feedback(PID_controller*sys1,1)
+step(m2); hold on; step(opt_pid_step); hold on; step(sys1);
+title(['PI controler (Kp = ' num2str(Kp2) ', Ki = ' num2str(Ti1) ').']);
+legend('PI_step','Tuned_PI_step','Sys1');
 
-%pid controller
-Kp = (0.6*Tb)/(K*Te);
-Ti = Tb;
+        %pid controller
+%///Parameters
+Kp3 = (0.6*Tb)/(K*Te);
+Ti2 = Tb;
 Td = 0.5*Te;
-pid_ctrl = pidstd(Kp, Ti, Td);
+%\\\
+PID_controller = pidstd(Kp3, Ti2, Td)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 subplot(3,1,3),
-m = feedback(pid_ctrl*sys1,1);
-step(m);
-title('PID controller')
+m3 = feedback(PID_controller*sys1,1)
+step(m3); hold on; step(opt_pid_step); hold on; step(sys1);
+title(['PID controler (Kp = ' num2str(Kp3) ', Ki = ' num2str(Ti2) ', Kd = ' num2str(Td) ').']);
+legend('PID_step','Tuned_PID_step','Sys1');
 
-% ********* 20% overshoot ***********
-%p controller
-Kp = (0.7*Tb)/(K*Te);
-pid_ctrl = pidstd(Kp);
+
+
+%% ********* 20% overshoot ***********
+fprintf('\n\t\t20 percent overshoot\n');
+        %p controller
+%///Parameters
+Kp1 = (0.7*Tb)/(K*Te);
+%\\\
+PID_controller = pidstd(Kp1)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 figure('name', '20% Overshoot')
 subplot(3,1,1),
-m1 = feedback(pid_ctrl*sys1,1)
-step(m1);
-title('P controller')
+m1 = feedback(PID_controller*sys1,1)
+step(m1); 
+hold on;
+step(opt_pid_step); 
+hold on;
+step(sys1);
+title(['P controler (Kp = ' num2str(Kp1) ').']);
+legend('P_step','Tuned_P_step','Sys1');
 
-%pi controller
-Kp = (0.6*Tb)/(K*Te);
-Ti = Tb;
-pid_ctrl = pidstd(Kp, Ti);
+        %pi controller
+%///Parameters
+Kp2 = (0.6*Tb)/(K*Te);
+Ti1 = Tb;
+%\\\
+PID_controller = pidstd(Kp2, Ti1)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 subplot(3,1,2),
-m2 = feedback(pid_ctrl*sys1,1)
-step(m2);
-title('PI controller')
+m2 = feedback(PID_controller*sys1,1)
+step(m2); hold on; step(opt_pid_step); hold on; step(sys1);
+title(['PI controler (Kp = ' num2str(Kp2) ', Ki = ' num2str(Ti1) ').']);
+legend('PI_step','Tuned_PI_step','Sys1');
 
-%pid controller
-Kp = (1.2*Tb)/(K*Te);
-Ti = 1.4*Tb;
+        %pid controller
+%///Parameters
+Kp3 = (1.2*Tb)/(K*Te);
+Ti2 = 1.4*Tb;
 Td = 0.47*Te;
-pid_ctrl = pidstd(Kp, Ti, Td);
+%\\\
+PID_controller = pidstd(Kp3, Ti2, Td)
+%****optimized controller
+Options = pidtuneOptions('PhaseMargin',75);
+tuned_ctrl = pidtune(sys1, PID_controller, Options);
+opt_pid_step = feedback(sys1*tuned_ctrl,1);
+%*****
+%plots
 subplot(3,1,3),
-m3 = feedback(pid_ctrl*sys1,1)
-step(m3);
-title('PID controller')
+m3 = feedback(PID_controller*sys1,1)
+step(m3); hold on; step(opt_pid_step); hold on; step(sys1);
+title(['PID controler (Kp = ' num2str(Kp3) ', Ki = ' num2str(Ti2) ', Kd = ' num2str(Td) ').']);
+legend('PID_step','Tuned_PID_step','Sys1');
