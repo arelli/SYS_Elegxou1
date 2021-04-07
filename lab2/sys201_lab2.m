e951073287ed472779f0523d4710b01a1e109818
TheApo98 <apogioum.ag@gmail.com>
2021-04-07 21:36:28 +0300

    Added some plots

--- lab2/sys201_lab2.m
+++ lab2/sys201_lab2.m
@@ -7,19 +7,32 @@ T2 = 3.33*Te;
 
 sys  = Ks * tf(1, [T1 1]) * tf(1, [T2 1])
 
-%///Parameters
-disp('p')
-Kp1 = (0.3*Tb)/(Ks*Te);
-%\\\
+figure('Name','Transfer function')
+% subplot(2,1,1);
+step(sys);
+title('Step Response.');
+xlabel('Time'); 
+
+% %///Parameters
+% disp('p')
+% Kp1 = (0.3*Tb)/(Ks*Te);
+% %\\\
 
 %///Parameters
 disp('pi')
-Kp2 = (0.35*Tb)/(Ks*Te);
-Ti1 = 1.2*Tb;
+Kp = (0.35*Tb)/(Ks*Te);
+Ti = 1.2*Tb;
 %\\\
-%///Parameters
-disp('pid')
-Kp3 = (0.6*Tb)/(Ks*Te);
-Ti2 = Tb;
-Td = 0.5*Te;
-%\\\
+figure('Name','PI controller')
+PID_controller = pidstd(Kp, Ti);
+m3 = feedback(PID_controller*sys,1)
+step(m3)
+title(['PI controler (Kp = ' num2str(Kp) ', Ki = ' num2str(Ti)  ').']);
+legend('PID_step')
+
+% %///Parameters
+% disp('pid')
+% Kp3 = (0.6*Tb)/(Ks*Te);
+% Ti2 = Tb;
+% Td = 0.5*Te;
+% %\\\
