3743a1fdfded8ec09e2913d50979df658072556a
TheApo98 <apogioum.ag@gmail.com>
2021-05-09 22:45:14 +0300

    Added some comments

--- lab3/sys201_lab3_1.m
+++ lab3/sys201_lab3_1.m
@@ -44,10 +44,10 @@ Kp1 = (0.7*Tg)/(Ks*Tu);
 %\\\
 PID_controller = pidstd(Kp1);
 tf(PID_controller)
+m1 = feedback(PID_controller*sys,1)
 %plots
 figure('name', '20% Overshoot')
 subplot(3,1,1),
-m1 = feedback(PID_controller*sys,1)
 step(m1); 
 hold on;
 step(sys);
@@ -62,9 +62,9 @@ Ti1 = Tg;
 %\\\
 PID_controller = pidstd(Kp2, Ti1);
 tf(PID_controller)
+m2 = feedback(PID_controller*sys,1)
 %plots
 subplot(3,1,2),
-m2 = feedback(PID_controller*sys,1)
 step(m2); 
 hold on; 
 step(sys);
@@ -83,16 +83,16 @@ Td = 0.47*Tu;
 %\\\
 PID_controller = pidstd(Kp3, Ti2, Td);
 tf(PID_controller)
+m3 = feedback(PID_controller*sys,1)
 %plots
 subplot(3,1,3),
-m3 = feedback(PID_controller*sys,1)
 step(m3); 
 hold on; 
 step(sys);
 title(['PID controler (Kp = ' num2str(Kp3) ', Ki = ' num2str(Ti2) ', Kd = ' num2str(Td) ').']);
 legend('PID_step','Sys');
 
-
+%input
 t1=0:0.01:900;
 for k=1:length(t1)
     if(t1(k)<0)
@@ -103,6 +103,8 @@ for k=1:length(t1)
         y1(k) = 5;
     end
 end
+
+% p response
 figure('Name','Step response')
 subplot(3,1,1),
 plot(t1, y1)
@@ -116,6 +118,7 @@ xlabel('Time (s)');
 ylabel('Nw (rpm)'); 
 e = resp - (y1.');
 
+% pi response
 subplot(3,1,2),
 plot(t1, y1)
 axis([0 900 0 10])
@@ -128,6 +131,7 @@ xlabel('Time (s)');
 ylabel('Nw (rpm)'); 
 e = resp - (y1.');
 
+% pid response
 subplot(3,1,3),
 plot(t1, y1)
 axis([0 900 0 10])
